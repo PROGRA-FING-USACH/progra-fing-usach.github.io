864266297322fe28ae0f71c37fee9ef3b71876da
PROGRA-FING-USACH <121065231+PROGRA-FING-USACH@users.noreply.github.com>
2023-03-13 17:52:55 -0300

    Update estilo.js

--- estilo.js
+++ estilo.js
@@ -1,11 +1,11 @@
 function definirEstilos(){
 	var estilo = document.createElement("STYLE");
 	var texto = document.createTextNode("\
-		@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,900;1,300&display=swap');\
+		@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');\
 		#pTitulo{\
 			font-size: 24px;\
 			font-family: 'Roboto', sans-serif;\
-			font-weight: 300;\
+			font-weight: lighter;\
 		}\
 		#pTitulo2{\
 			font-size: 22px;\
